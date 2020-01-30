2d65241a8c88c6b47f64ab4a1ba1e40ed8ebc8b2
aeminkyr <aeminkyr@gmail.com>
2020-01-30 14:44:21 +0300

    parçalama işlemi ve autoload ayarları yapıldı

---
+++ application/views/components/navbar.php
@@ -0,0 +1,23 @@
+<nav class="navbar sticky-top navbar-light border-bottom">
+		<a class="navbar-brand" id="site-logo" href="#" style="color:black;" >8 BIT DICTIONARY</a>
+		<a class="navbar-brand" id="site-logo-m" href="#" style="color:black;" >8BD</a>
+		<form id="search-form">
+			<input type="text" placeholder="Search..">
+			<button type="submit"><i class="fa fa-search"></i></button>
+		</form>
+		<a style="float: right;" href="#login">login</a>
+	</nav>
+	<ul class="nav" id="nav-items" style="">
+		<li class="nav-item">
+			<a class="nav-link active" href="#">#objectoriented</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="#">#devops</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="#">#network</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="#" tabindex="-1" >#something</a>
+		</li>
+	</ul>
