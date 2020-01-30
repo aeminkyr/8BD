863b4e130537694ae940cde7186dc6849a99bcef
aeminkyr <aeminkyr@gmail.com>
2020-01-30 15:53:30 +0300

    bootstrap ve js kaynakları dahil edildi

--- application/views/dependencies/css.php
+++ application/views/dependencies/css.php
@@ -1,7 +1,4 @@
-<!--<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/bootstrap.min.css");?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/main.css")?>" >-->
-
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.min.css");?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/main.css")?>">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" integrity="sha256-+N4/V/SbAFiW1MPBCXnfnP9QSN3+Keu+NlB+0ev/YKQ=" crossorigin="anonymous" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 
